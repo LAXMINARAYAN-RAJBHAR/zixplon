524c20ef8f3c35fdb2f52e14ec6c2f241d5d03ba
Laxminarayan Rajbhar <rajbhar.laxminarayan01@gmail.com>
2026-06-03 14:58:01 +0530

    update: new changes

--- src/ScrollToTop.jsx
+++ src/ScrollToTop.jsx
@@ -2,23 +2,10 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const location = useLocation();
-
+  const { pathname } = useLocation();
   useEffect(() => {
-    // only use scrollIntoView on video page
-    if (location.pathname.startsWith("/video/")) {
-      setTimeout(() => {
-        const player = document.querySelector(".video_player_wrapper");
-        if (player) {
-          player.scrollIntoView({ behavior: "smooth", block: "start" });
-        }
-      }, 100);
-    } else {
-      // all other pages scroll to top normally
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    }
-  }, [location.pathname, location.key]);
-
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
   return null;
 };
 

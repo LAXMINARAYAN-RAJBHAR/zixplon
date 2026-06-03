5a281ce9655a14c96acd09f795584f97cc708f42
Laxminarayan Rajbhar <rajbhar.laxminarayan01@gmail.com>
2026-06-03 15:04:22 +0530

    update: new changes

--- src/ScrollToTop.jsx
+++ src/ScrollToTop.jsx
@@ -3,9 +3,22 @@ import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
+  
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    // Try all possible scrollable containers
+    window.scrollTo(0, 0);
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    
+    // Target the App container specifically for mobile
+    const app = document.querySelector(".App");
+    if (app) app.scrollTop = 0;
+    
+    const homePage = document.querySelector(".homePage");
+    if (homePage) homePage.scrollTop = 0;
+
   }, [pathname]);
+  
   return null;
 };
 

0dd6547f61f210fbd2ee1011160cd664536e46bd
Laxminarayan Rajbhar <rajbhar.laxminarayan01@gmail.com>
2026-06-03 13:27:30 +0530

    update: new changes

--- src/Pages/Video/video.js
+++ src/Pages/Video/video.js
@@ -897,11 +897,18 @@ const Video = () => {
   }, [id]);
 
   useEffect(() => {
-    setDisliked(false);
-    setVideoError(false);
-    setIsVideoPlaying(false);
-    window.scrollTo(0, 0);
-  }, [id]);
+  setDisliked(false);
+  setVideoError(false);
+  setIsVideoPlaying(false);
+  // scroll everything possible
+  window.scrollTo(0, 0);
+  document.documentElement.scrollTo(0, 0);
+  document.body.scrollTo(0, 0);
+  const videoEl = document.querySelector(".video");
+  if (videoEl) videoEl.scrollTo(0, 0);
+  const suggestions = document.querySelector(".videoSuggestions");
+  if (suggestions) suggestions.scrollTo(0, 0);
+}, [id]);
 
   if (dbLoading) {
     return (

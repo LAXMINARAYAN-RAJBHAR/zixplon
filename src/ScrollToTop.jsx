21f0921b37facb47179fcab82f6a7eedff621edb
Laxminarayan Rajbhar <rajbhar.laxminarayan01@gmail.com>
2026-06-03 13:17:20 +0530

    update: new chnages

--- src/ScrollToTop.jsx
+++ src/ScrollToTop.jsx
@@ -2,10 +2,10 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const location = useLocation();
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
-  }, [pathname]);
+  }, [location.pathname, location.key]);  // key changes on every navigation
   return null;
 };
 
